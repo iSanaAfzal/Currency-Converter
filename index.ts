8979dfcef06b781c891926d0713ab285d2687540
sanuafzal <sanafzal.0985@gmail.com>
2024-02-23 10:17:23 +0500

    currencyConverter

---
+++ index.ts
@@ -0,0 +1,57 @@
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+
+let convertion = {
+    "PKR": {
+        "USD": 0.0044,
+        "GBP": 0.0037,
+        "PKR": 1,
+    },
+    "GBP": {
+        "USD": 1.121,
+        "PKR": 271.79,
+        "GBP": 1,
+    },
+    "USD": {
+        "PKR": 225.50,
+        "GBP": 0.83,
+        "USD": 1,
+    }
+}
+
+async function main() {
+    const answers: {
+        from: "PKR" | "USD" | "GBP",
+        to: "PKR" | "USD" | "GBP",
+        amount: number,
+    } = await inquirer.prompt([
+        {
+            type: "list",
+            name: "from",
+            choices: ["PKR", 'USD', 'GBP'],
+            message: "Select Your Currency?\n"
+        },
+        {
+            type: "list",
+            name: "to",
+            choices: ["PKR", 'USD', 'GBP'],
+            message: "Select Your  conversion Currency?\n"
+        },
+        {
+            type: "number",
+            name: "amount",
+            message: "Enter Your  conversion amount?\n"
+        }
+    ]);
+
+    const { from, to, amount } = answers;
+
+    if (from && to && amount) {
+        let result = convertion[from][to] * amount;
+        console.log(chalk.green(`Your Conversion from ${chalk.bold(from)} to ${chalk.bold(to)} is: ${chalk.bold(result)}`));
+    } else {
+        console.log(chalk.red('Invalid Inputs'));
+    }
+}
+
+main();
